5ad88a5f20fe3492f2843f11acd1f20a368864f5
Yasutaka Kawamoto <kwmt27@gmail.com>
2016-12-02 23:24:05 +0900

    add gozen

---
+++ gopath/src/gozen/controllers/user_controller.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"gozen/models"
+	"gozen/models/json"
+)
+
+// ユーザーコントローラー
+type UserController struct{}
+
+// 登録情報を返す
+func (user UserController) Profile() gin.HandlerFunc {
+	return jsonController(func(c *gin.Context) (interface{}, models.Error) {
+		if models.LoginUser == nil {
+			return nil, models.NewError(http.StatusBadRequest, "ログインしてください。")
+		}
+		return models.LoginUser.ToJson(), nil
+	})
+}
+
+// registration idを登録する
+func (user UserController) RegistrationID() gin.HandlerFunc {
+	return jsonController(func(c *gin.Context) (interface{}, models.Error) {
+		if models.LoginUser == nil {
+			return nil, models.NewError(http.StatusBadRequest, "ログインしてください。")
+		}
+
+		regID := c.PostForm("registration_id")
+		if regID == "" {
+			return nil, models.NewError(http.StatusNotAcceptable, "not registraion_id key")
+		}
+		user := models.NewUser()
+		if err := user.SaveRegistrationID(regID); err != nil {
+			return nil, models.NewError(http.StatusInternalServerError, err.Error())
+		}
+		return json.MessageResponse{
+			"saved registration id",
+		}, nil
+	})
+}
